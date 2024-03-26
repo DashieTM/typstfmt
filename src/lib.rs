86ebce0f8cf249df0dde82739d7c60428e60e7c5
Fabio Lenherr / DashieTM <fabio.lenherr@gmail.com>
2024-03-26 11:09:39 +0100

    dkafgh

--- src/lib.rs
+++ src/lib.rs
@@ -73,10 +73,9 @@ fn visit(node: &LinkedNode, ctx: &mut Ctx) -> String {
         }
         LetBinding => format_let_binding(node, &res, ctx),
         Conditional => conditional_format(node, &res, ctx),
-        Raw | BlockComment => {
-            if ctx.config.format_code {
-                ctx.lost_context();
-            }
+        Raw => code_blocks::format_code_blocks(node, &res, ctx),
+        BlockComment => {
+            ctx.lost_context();
             node.text().to_string()
         }
         Equation => math::format_equation(node, &res, ctx),
